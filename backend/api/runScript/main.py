c0ebb264ca9274f1c42fe99be0d8a3d91ba49a17
Mridul Garg <mridulgarg.mg@gmail.com>
2020-08-21 09:10:30 +0530

    Introducing Docker Run Functional

--- backend/api/runScript/main.py
+++ backend/api/runScript/main.py
@@ -10,15 +10,17 @@ def execute():
     incomingData = request.get_json()
     print(incomingData)
     lang = incomingData['language']
-    inputFile = incomingData['script']
-    command = 'python ' + lang + '.py ' + inputFile
+    inputFile = incomingData['script'] 
+    # command = 'python ' + lang + '.py ' + inputFile
+    sudoPass = 'mridmehu' #host sudo pass
+    commandDocker = 'sudo docker run --rm pascal:python_slim python ' + 'interpreter' + '.py ' + inputFile
     print("=="*50)
-    print(command)
+    print(commandDocker)
     print (str(langDir/lang))
     print("==" * 50)
     try:
-        output = runCommandCMD(directory=langDir/lang, command=command, timeout=100)
+        output = runCommandCMD(directory=langDir/lang, command=commandDocker, timeout=100, sudoPass=sudoPass)
     except:
         return ("yoooooo!")
-    outputDict = {'stdout': str(output[0]), 'stderr': str(output[1])}
+    outputDict = {'stdout': output[0], 'stderr': output[1]}
     return jsonify(outputDict)
